f6ae27fa844b469d1523fa18f3751e0e130d50dd
Maharaja <maharaja@crayondata.com>
2022-02-07 22:45:46 +0530

    Added object creation method

--- src/main/java/com/createCombination/workingMagic.java
+++ src/main/java/com/createCombination/workingMagic.java
@@ -2,29 +2,31 @@ package com.createCombination;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.paukov.combinatorics3.Generator;
 
-import com.google.common.collect.Sets;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.utilities.readFile;
 
-public class magic3 {
+public class workingMagic {
 
 	public static void main(String args[]) throws FileNotFoundException {
 		readFile read = new readFile();
-		JsonObject obj = read.readJson("testData.json");	
+		JsonObject obj = read.readJson("testData.json");
+		System.out.println(consolidatedList(obj));
 		List<List<String>> listString = printCombinations(consolidatedList(obj), obj);
 		List<List<String>>output=filterCombinations(listString, nodeObjectList(obj), obj);
+		System.out.println(listString);
+		System.out.println(output.size());
 		System.out.println(output);
+		System.out.println("<================================>");
+		System.out.println(creatJsonArray(output,obj));
 	}
 	
 	static List<List<String>> nodeObjectList(JsonObject obj)
@@ -64,7 +66,6 @@ public class magic3 {
 	static List<List<String>> filterCombinations(List<List<String>> sequence, List<List<String>> simpleList, JsonObject obj) {
 
 		List<List<String>> filteredList = new ArrayList<List<String>>();
-		System.out.println(sequence.size());
 		for (int i = 0; i < sequence.size(); i++) {
 			int count = 1;
 			for (int j = 0; j < simpleList.size(); j++) {
@@ -85,14 +86,42 @@ public class magic3 {
 
 	}
 
-	static boolean compareTwoList(List<String> list1, List<String> list2) {
+	static boolean compareTwoList(List<String> list1, List<String> list2) 
+	{
 		boolean flag = false;
 		List<String> tempx = new ArrayList<String>(list1);
 		List<String> tempy = new ArrayList<String>(list2);
+		if(tempx.size()>=tempy.size())
+		{
 		tempx.retainAll(tempy);
 		if (tempx.size() == 1) {
 			flag = true;
 		}
+		}
+		else if(tempx.size()<tempy.size())
+		{
+			tempy.retainAll(tempx);
+			if (tempy.size() == 1) {
+				flag = true;
+			}
+		}
+		
 		return flag;
+	}
+	static JsonArray creatJsonArray(List<List<String>> list,JsonObject obj)
+	{
+		List<String> set=obj.keySet().stream().collect(Collectors.toList());
+		JsonArray arr=new JsonArray();
+		for(List<String> l:list)
+		{
+			JsonObject object=new JsonObject();
+			for(int i=0;i<l.size();i++)
+			{
+			object.addProperty(set.get(i), l.get(i));
+			}
+			arr.add(object);
+		}
+		
+		return arr;
 	}
 }
